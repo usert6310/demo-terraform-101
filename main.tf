a7b4baf0fabdf0efd4afc379cdf8bba56a0f168d
usert6310 <115548759+usert6310@users.noreply.github.com>
2022-10-11 12:54:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
 
 resource "aws_instance" "web" {
-  # ...
+  ami = var.ami
+  instance_type = "t2.micro"
 }
